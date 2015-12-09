2cdbd4a4bcb989ce0d30a7ce6dda85bbffabee55
Sebastian Karlsson <=>
2015-12-09 08:36:18 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name='LWPCMS',
+    version='1.0',
+    description='Lightweight Python Content Management System',
+    author='Sebastian Robert Karlsson',
+    author_email='sebbekarlsson97@gmail.com',
+    url='http://www.ianertson.com/',
+    install_requires=[
+        'flask',
+        'flask-wtf',
+        'wtforms',
+        'PyMySQL',
+        'sqlalchemy',
+        'pyyaml'
+    ]
+)
