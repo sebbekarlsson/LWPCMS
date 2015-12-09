6e5ef62e06e75aeb5bdc87514243be987790b48e
Sebastian Karlsson <=>
2015-12-09 12:33:09 +0100

    Admin sidebar is now built from json

--- lwpcms/views/admin/admin.py
+++ lwpcms/views/admin/admin.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, abort
 
+import json
+
 
 bp = Blueprint(
     __name__, __name__,
@@ -8,4 +10,7 @@ bp = Blueprint(
 
 @bp.route('/admin')
 def render():
-    return render_template('admin.html')
+    with open('lwpcms/static/shards/admin_side_nav.json') as file:
+        side_nav_buttons = json.loads(file.read())
+    
+    return render_template('admin.html', side_nav_buttons=side_nav_buttons)
