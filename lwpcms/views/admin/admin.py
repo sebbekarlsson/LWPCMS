5d500e759a41cbb7e97a84d39e61fe6229bc31bc
Sebastian Karlsson <=>
2015-12-09 13:04:04 +0100

    Admin sidenav is now more modular. The view reads all json files in the /static/shards/admin/side_nav/ dir

--- lwpcms/views/admin/admin.py
+++ lwpcms/views/admin/admin.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, abort
 
+import glob
+
 import json
 
 
@@ -10,7 +12,11 @@ bp = Blueprint(
 
 @bp.route('/admin')
 def render():
-    with open('lwpcms/static/shards/admin_side_nav.json') as file:
-        side_nav_buttons = json.loads(file.read())
+    side_nav_buttons = []
+
+    for json_file in glob.glob('lwpcms/static/shards/admin/side_nav/*.json'):
+        with open(json_file) as file:
+            side_nav_buttons += json.loads(file.read())['buttons']
+            
     
     return render_template('admin.html', side_nav_buttons=side_nav_buttons)
