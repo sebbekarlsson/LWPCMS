6692be0c63166666685cb4247aa4db4b9e2dd9e5
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2015-12-09 19:50:19 +0100

    Added each section in the admin navbar and made the admin navbar a little bit smaller

--- lwpcms/views/admin/admin.py
+++ lwpcms/views/admin/admin.py
@@ -7,12 +7,53 @@ import json
 
 bp = Blueprint(
     __name__, __name__,
-    template_folder='templates'
+    template_folder='templates',
+    url_prefix='/admin'
 )
 
-@bp.route('/admin')
+@bp.route('/')
 def render():
     with open('lwpcms/static/shards/admin/side_nav.json') as file:
         side_nav_data = json.loads(file.read())
 
     return render_template('admin.html', side_nav_data=side_nav_data)
+
+
+@bp.route('/publish')
+def render_publish():
+    with open('lwpcms/static/shards/admin/side_nav.json') as file:
+        side_nav_data = json.loads(file.read())
+
+    return render_template('admin_publish.html', side_nav_data=side_nav_data)
+
+
+@bp.route('/users')
+def render_users():
+    with open('lwpcms/static/shards/admin/side_nav.json') as file:
+        side_nav_data = json.loads(file.read())
+
+    return render_template('admin_users.html', side_nav_data=side_nav_data)
+
+
+@bp.route('/modules')
+def render_modules():
+    with open('lwpcms/static/shards/admin/side_nav.json') as file:
+        side_nav_data = json.loads(file.read())
+
+    return render_template('admin_modules.html', side_nav_data=side_nav_data)
+
+
+@bp.route('/files')
+def render_files():
+    with open('lwpcms/static/shards/admin/side_nav.json') as file:
+        side_nav_data = json.loads(file.read())
+
+    return render_template('admin_files.html', side_nav_data=side_nav_data)
+
+
+@bp.route('/settings')
+def render_settings():
+    with open('lwpcms/static/shards/admin/side_nav.json') as file:
+        side_nav_data = json.loads(file.read())
+
+    return render_template('admin_settings.html', side_nav_data=side_nav_data)
