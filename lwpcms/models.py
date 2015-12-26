a39f9c5e797391d33493131d8dfa0eeba8f66a15
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2015-12-26 21:27:09 +0100

    Some drastic style changes. This is more clean, I think...

---
+++ lwpcms/models.py
@@ -0,0 +1,44 @@
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+
+Base = declarative_base()
+engine = sa.create_engine('sqlite:///{}.sqlite'.format(db_name),
+        connect_args={'check_same_thread': False})
+
+
+def initialize_database():
+    Base.metadata.create_all(engine)
+
+
+def new_session():
+    
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+
+    return Session()
+
+
+class Data():
+    created = sa.Column(sa.TIMESTAMP, server_default=sa.func.now(),
+            onupdate=sa.func.current_timestamp())
+
+
+class User(Base, Data):
+    __tablename__ = 'users'
+    id = sa.Column(sa.Integer, primary_key=True)
+    first_name = sa.Column(sa.String(128))
+    last_name = sa.Column(sa.String(128))
+    password = sa.Column(sa.String(256))
+
+
+class Post(Base, Data):
+    __tablename__ = 'posts'
+    id = sa.Column(sa.Integer, primary_key=True)
+    title = sa.Column(sa.String(256))
+    content = sa.Column(sa.String(1024))
+    author = relationship("User", uselist=False, backref="posts")
+
+
+sess = new_session()
