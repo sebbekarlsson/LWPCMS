6a231c86035c36200e70559c3143586d5d0bcce8
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-02-02 09:50:26 +0100

    moved function

--- lwpcms/static/js/functions.js
+++ lwpcms/static/js/functions.js
@@ -31,6 +31,26 @@ function remove_attachment(post_id, attach_id) {
 /*
  * This will be converted to a more convincing httprequest library later.
 */
+function delete_file(e, id) {
+    var http = new XMLHttpRequest();
+    var url = "/api/delete_file/" + id;
+
+    http.open("GET", url, true);
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            e.parentNode.parentNode.style.display = 'none';
+        }
+    }
+
+    http.send();
+}
+
+
+/*
+ * This will be converted to a more convincing httprequest library later.
+*/
 function generate_testdata() {
     var http = new XMLHttpRequest();
     var url = "/api/generate_testdata";
