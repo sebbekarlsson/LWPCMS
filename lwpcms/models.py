8663223f08d3b3183d99b480f251fe4056125a9d
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-02-24 19:51:52 +0100

    added models

--- lwpcms/models.py
+++ lwpcms/models.py
@@ -8,14 +8,16 @@ class DBObject(object):
     def __init__(
             self,
             classes=[],
-            type=''
-        ):
+            type='',
+            meta={}
+            ):
         self.created = time.strftime("%Y:%m:%d:%H:%M:%S")
         self.updated = time.strftime("%Y:%m:%d:%H:%M:%S")
         self.classes = classes
         self.type = type
+        self.meta = meta
         self.structure = self.__class__.__name__
-    
+
 
     def export(self):
         """This function exports the object as a dict. This is used when
@@ -25,20 +27,54 @@ class DBObject(object):
 
 
 class Post(DBObject):
-    
+
     def __init__(
             self,
             title='',
             content='',
             attachments=[],
             author={},
-            type='',
             classes=[],
+            type='',
             meta={}
-        ):
-        DBObject.__init__(self, classes, type)
+            ):
+        DBObject.__init__(self, classes, type, meta)
         self.title = title
         self.content = content
         self.attachments = attachments
         self.author = author
         self.meta = meta
+
+
+class User(DBObject):
+
+    def __init__(
+            self,
+            nick_name='',
+            first_name='',
+            last_name='',
+            password='',
+            classes=[],
+            type='',
+            meta={}
+            ):
+        DBObject.__init__(self, classes, type, meta)
+        self.nick_name = nick_name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+
+class Option(DBObject):
+
+    def __init__(
+            self,
+            key='',
+            value='',
+            classes=[],
+            type='',
+            meta={}
+            ):
+        DBObject.__init__(self, classes, type, meta)
+        self.key = key
+        self.value = value
