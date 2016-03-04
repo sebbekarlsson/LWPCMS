8c0c812971d80faec15e976dc24fd2b009396ec6
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-03-04 21:17:56 +0100

    removed generate_testdata function

--- lwpcms/static/js/functions.js
+++ lwpcms/static/js/functions.js
@@ -46,18 +46,3 @@ function delete_file(e, id) {
 
     http.send();
 }
-
-
-/*
- * This will be converted to a more convincing httprequest library later.
-*/
-function generate_testdata() {
-    var http = new XMLHttpRequest();
-    var url = "/api/generate_testdata";
-
-    http.open("GET", url, false);
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    http.send();
-
-    return JSON.parse(http.responseText);
-}
