9d2232d7f0c899de89a527ba18dbce9aa9c448a1
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-03-05 12:06:59 +0100

    created python function for making remote-queries

--- lwpcms/api/utils.py
+++ lwpcms/api/utils.py
@@ -4,11 +4,5 @@ import json
 
 def lwpcms_query_remote(remote_url, query):
     response = requests.post(remote_url + 'api/query', json.dumps(query), headers={'Content-Type': 'application/json'})
-    print(response.text)
-
-
-#lwpcms_query_remote('http://demo.lwpcms.com/', {
-#        'query': {
-#            'structure': '#User'
-#            }
-#    })
+    
+    return json.loads(response.text)
