aa3814c0cfbfaf409fbd0dbad16c1f72e5d230c2
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-03-06 18:26:24 +0100

    Wrote APIWrapper [Not done yet]

---
+++ lwpcms/api/APIWrapper.py
@@ -0,0 +1,22 @@
+from urllib.parse import urljoin
+import requests
+import json
+
+
+class APIWrapper(object):
+    
+
+    def __init__(self, host):
+        self.host = host
+        self.query_endpoint = urljoin(host, 'api/query')
+        self.headers = {'Content-Type': 'application/json'}
+
+
+    def query(self, query):
+        response = requests.post(
+                self.query_endpoint,
+                json.dumps(query),
+                headers=self.headers
+                )
+        
+        return json.loads(response.text)
--- lwpcms/api/utils.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-
-def lwpcms_query_remote(remote_url, query):
-    response = requests.post(remote_url + 'api/query', json.dumps(query), headers={'Content-Type': 'application/json'})
-    
-    return json.loads(response.text)
