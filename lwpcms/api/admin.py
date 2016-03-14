3548908a41301704b53b937aa5b712c8edf8d7e6
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-03-14 21:59:06 +0100

    test

--- lwpcms/api/admin.py
+++ lwpcms/api/admin.py
@@ -4,7 +4,7 @@ from lwpcms.api.constants import hooks
 
 
 def get_sidenav():
-    with open('lwpcms/static/shards/admin/side_nav.json') as file:
+    with open('./lwpcms/static/shards/admin/side_nav.json') as file:
             nav = json.loads(file.read())
 
             call_module_event(hooks['admin_sidenav'], {'nav': nav})
